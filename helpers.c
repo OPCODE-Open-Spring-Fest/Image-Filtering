0d076815532ce1057791c5de0d1f063312c6c961
harshsankhla18 <harsh18sankhlamy@gmail.com>
2025-10-16 17:15:36 +0530

    feat: add helpers.c template for function definitions

---
+++ helpers.c
@@ -0,0 +1,29 @@
+#include "helper.h"
+
+
+void grayscale(int height, int width, RGBTRIPLE image[height][width]){
+
+// Convert image to grayscale
+
+}
+
+
+void sepia(int height, int width, RGBTRIPLE image[height][width]){
+
+// Convert image to sepia
+
+}
+
+
+void reflect(int height, int width, RGBTRIPLE image[height][width]){
+
+// Reflect image horizontally
+
+}
+
+
+void blur(int height, int width, RGBTRIPLE image[height][width]){
+
+// Blur image
+
+}
